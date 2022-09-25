911c3a95226c2476cb694e72b7be83b9dfb08d2f
dtwjy <dtan0050@student.monash.edu>
2022-09-25 19:26:06 +1000

    Initial commit

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "map2.json";
+vegaEmbed("#world_map", vg_1,{ "actions": false }).then(function(result) {
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
